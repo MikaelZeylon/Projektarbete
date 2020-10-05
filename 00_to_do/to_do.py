9ae39612479a5bc8a9ddce81040c9ef712945af4
Mikael Zeylon <mikael.zeylon@hotmail.com>
2020-10-05 10:29:17 +0200

    projekt

---
+++ 00_to_do/to_do.py
@@ -0,0 +1,44 @@
+# write as functions
+
+    # str to upper(), split()
+    drawing_name = drawing_name.upper().split()
+
+    # pop list to int / str
+    num = int(drawing_name.pop()); nam = drawing_name.pop()
+
+# write a class with current functions
+
+# csv / excel
+
+# openpyxl --> excel
+
+# link class from separate folder, os path
+
+# arg var, avvakta med denna ide
+
+# foor-loops som oneliners
+
+# vad är Driver Code?
+
+class Dog:  
+
+    animal = 'dog'      
+         
+    def __init__(self, breed):  
+        self.breed = breed              
+    
+    def setColor(self, color):  
+        self.color = color  
+            
+    def getColor(self):      
+        return self.color     
+    
+# Driver Code  
+Rodger = Dog("pug")  
+Rodger.setColor("brown")  
+print(Rodger.getColor())
+
+# 1. när ska man göra funktioner eller ej av kod? 
+# 2. hur kopplas skriptet ihop med en webbsida?
+
+# hur ska jag lopa hela funktionen för att få fram fler typer av planer? grund / tak mm
---
+++ 03_test/test.py
@@ -0,0 +1,102 @@
+# bygg ihop def name, drawing_name_part_1, drawing_name_part_2
+
+def name(var_1):
+    var_1 = var_1.upper().split()
+    num = int(var_1.pop())
+    nam = var_1.pop()
+
+    return num, nam
+
+def drawing_number_concatenate(_count, _list, var_1, var_2, var_3, var_4, 
+    var_5, var_6):
+    """ this function concatenates the whole drawing number,
+    for example 'K-20-1-100' """
+    for i in range(0, _count):
+        x = f"{var_1}-{var_2}-{var_3}-{var_4}{var_5}{var_6} - ".upper()
+        _list.append(x)
+
+    return _list
+
+def drawing_name_part_1(_count, _list, _var):
+    """ this function takes care of the first part of the drawing name, 
+    for example the part 'BÖP' of 'BÖP 10' """
+    for i in range(0, _count):
+        _list.append(_var)
+
+    return _list
+
+def drawing_name_part_2(_count, _list, _var):
+    """ this function takes care of the last part of the drawing name,
+    for example the part '10' of 'BÖP 10' """
+    for i in range(0, _count):
+        _list.append(_var)
+        _var += 1
+
+    return _list
+
+def drawing_name_concatenate(_count, list_1, list_2, list_3):
+    """ this function concatenates drawing_name_part_1 and drawing_name_part_2,
+    for example the part 'BÖP' and '10' concatenates to 'BÖP 10' """
+    for i in range(0, _count):
+        x = list_1.pop(0)
+        y = list_2.pop(0)
+        z = f"{x} {y}"
+        list_3.append(z)
+
+    return list_3
+
+def drawing_number_and_name_concatenate(_count, list_1, list_2, list_3):
+    """ this function concatenates  drawing_number_concatenate and 
+    drawing_name_concatenate,
+    for example the part 'BÖP' and '10' concatenates to 'BÖP 10' """
+    for i in range(0, _count):
+        x = list_1.pop(0)
+        y = list_2.pop(0)
+        z = f"{x}{y}"
+        list_3.append(z)
+
+    return list_3
+
+discipline = input("Ange diciplin: ")
+drawing_content = input("Ange ritningens innehåll: ")
+drawing_category = input("Ange ritningens kategori: ")
+house_number = input("Ange husnummer: ")
+house_part = input("Ange husdel: ")
+house_floor = input("Ange våning: ")
+drawing_name = input("Ange ritnignens namn: ")
+number_of_floors = int(input("Ange antal våningar: "))  
+
+# empty lists
+list_drawing_number_concatenate = [] 
+list_drawing_name_part_1 = []
+list_drawing_name_part_2 = []
+list_drawing_name_concatenate = []
+list_drawing_number_and_name_concatenate = []
+
+# unpack var in name
+num, nam = name(drawing_name)
+print(f"this is num: {num}")
+print(f"this is nam: {nam}")
+
+# use func
+list_drawing_number_concatenate = drawing_number_concatenate(
+    number_of_floors, list_drawing_number_concatenate, discipline, 
+    drawing_content, drawing_category, house_number, house_part, house_floor
+    )
+list_drawing_name_part_1 = drawing_name_part_1(
+    number_of_floors, list_drawing_name_part_1, nam
+    )
+list_drawing_name_part_2 = drawing_name_part_2(
+    number_of_floors, list_drawing_name_part_2, num
+    )
+list_drawing_name_concatenate = drawing_name_concatenate(
+    number_of_floors, list_drawing_name_part_1, list_drawing_name_part_2,
+    list_drawing_name_concatenate
+    )
+list_drawing_number_and_name_concatenate = drawing_number_and_name_concatenate(
+    number_of_floors, list_drawing_number_concatenate, 
+    list_drawing_name_concatenate, list_drawing_number_and_name_concatenate
+    )
+
+for i in range (0, number_of_floors):
+    print(list_drawing_number_and_name_concatenate.pop(0))
